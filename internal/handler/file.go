622f4cc952a0b2afb7d933af5abb127160a9b386
Winson <winson@qq.com>
2024-11-14 22:13:17 +0800

    feat(file): 修改获取全部文件方法

--- internal/handler/file.go
+++ internal/handler/file.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/wxnacy/bdpan"
 	"github.com/wxnacy/bdpan-cli/internal/config"
 	"github.com/wxnacy/bdpan/file"
 )
@@ -20,6 +21,26 @@ type FileHandler struct {
 	acceccToken string
 }
 
-func (h *FileHandler) GetFileList() {
-	file.GetFileList(h.acceccToken, file.NewGetFileListReq())
+func (h *FileHandler) GetDirAllFiles(dir string) ([]*bdpan.FileInfoDto, error) {
+	req := file.NewGetFileListReq()
+	req.Dir = dir
+	totalList := []*bdpan.FileInfoDto{}
+	fileList := []*bdpan.FileInfoDto{}
+	page := 1
+	for {
+		req.SetPage(page)
+		res, err := file.GetFileList(h.acceccToken, req)
+		if err != nil {
+			return nil, err
+		}
+		fileList = res.List
+		totalList = append(totalList, fileList...)
+
+		if len(fileList) <= 0 || len(fileList) < int(req.Limit) {
+			break
+		}
+		page++
+	}
+	return totalList, nil
+
 }
