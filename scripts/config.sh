46423a8198bacad45708caff5642c334e3d5052c
wxnacy <wxnacy@gmail.com>
2025-03-06 10:39:35 +0800

    feat(scripts): config add doc url

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# https://github.com/go-dev-frame/sponge/blob/main/assets/install-cn.md
+# go install github.com/go-dev-frame/sponge/cmd/sponge@latest
 sponge config -f /Users/wxnacy/Documents/Configs/bdpan-cli/config/bdpan-cli.yml -o ./
